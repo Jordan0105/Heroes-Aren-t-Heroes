39fd69b3c6b6c87ada0dbe1e33d2e2b7c39caf61
Jordan0105 <jordanma2004@outlook.es>
2023-01-12 01:21:45 -0600

    The load and close for the 3 first screen are done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 
 //HTML Element Selector
 
-const characterDiv = document.getElementsByClassName("characterDiv");
-const characterImg = document.getElementById("characterImg");
-const body = document.getElementsByTagName("body")[0];
-const main = getComputedStyle(document.getElementById("main"));
-const bodyDimension = document.getElementsByTagName("body")[0];
-const hitBoxCharacterStyle = document.getElementById("characterHitBox");
+const startScreen = document.getElementById("startScreen");
+
+let characterDiv = document.getElementById("characterDiv");
+let characterImgSrc, characterAlt;
+let body = document.getElementsByTagName("body")[0];
+let main = getComputedStyle(document.getElementById("main"));
+let bodyDimension = document.getElementsByTagName("body")[0];
+let hitBoxCharacterStyle = document.getElementById("characterHitBox");
 
 let timerJump = null,
     positionJump = 0,
@@ -17,7 +19,6 @@ let timerJump = null,
     cloudInterval1,
     existCloud = false,
     timerStartContinue,
-    timerChooseCharacter,
     opacityAuraSetinterval,
     opacity = 0;
 
@@ -33,14 +34,186 @@ function randomTopCloud() {
 
 //Classes
 
-class Character {
-    constructor(characterImg) {
-        this.characterImg = characterImg;
+
+
+class Cloud {
+    constructor() {
+
     }
+}
+// const newCloud = new Cloud();
 
+//! Functions 
+
+const closeHomeScreen = () => {
+    clearInterval(timerStartContinue);
+    startScreen.style.display = "none";
+}
+
+const loadGameplayScreenEvent = () => {
+
+    //* Delete the previous screen
+
+    closeChooseCharacterScreen();
+
+    //* Load the Gameplay Screen
+
+    loadGameplayScreen();
+
+    window.removeEventListener("keydown", loadGameplayScreenEvent);
+
+}
+
+const loadChooseCharacterScreenEvent = () => {
+
+    //Load the select character screen
+    closeHomeScreen();
+    loadChooseCharacterScreen();
+    window.removeEventListener("keydown", loadChooseCharacterScreenEvent);
+}
+
+//TODO: Create the loadHomeScreen Function
+
+const closeChooseCharacterScreen = () => {
+    document.getElementById("chooseCharacter").style.display = "none";
+}
+
+const loadChooseCharacterScreen = () => {
+
+    document.getElementById("chooseCharacter").style.display = "flex";
+
+    for (let i = 0; i < document.getElementsByClassName("characterBoxSpace").length; i++) {
+
+        document.getElementsByClassName("characterBoxSpace")[i].addEventListener("mouseenter", (event) => {
+            // console.log(document.getElementsByClassName("characterBoxSpaceHidden")[i].id);
+            const aura = document.getElementById("aura");
+            clearInterval(opacityAuraSetinterval);
+            opacity = 0;
+            aura.style.opacity = opacity;
+
+            opacityAuraSetinterval = setInterval(() => {
+
+
+                aura.style.opacity = opacity;
+
+                if (aura.style.opacity <= 0.6) {
+                    opacity = opacity + 0.05;
+                    aura.style.opacity = opacity;
+                }
+                else {
+                    clearInterval(opacityAuraSetinterval);
+                    opacity = 0;
+
+                }
+
+
+
+            }, 100);
+
+            switch (i) {
+
+                case 0:
+                    aura.style.top = "-12px";
+                    aura.style.left = "56px";
+                    break;
+
+                case 1:
+                    aura.style.top = "-12px";
+                    aura.style.left = "488px";
+                    break;
+
+                case 2:
+                    aura.style.top = "-12px";
+                    aura.style.left = "918px";
+                    break;
+
+                //Characters 2nd row
+
+                case 3:
+                    aura.style.top = "255px";
+                    aura.style.left = "56px";
+                    break;
+
+                case 4:
+                    aura.style.top = "255px";
+                    aura.style.left = "494px";
+                    break;
+
+                case 5:
+                    aura.style.top = "255px";
+                    aura.style.left = "922px";
+                    break;
+            }
+        });
+
+        document.getElementsByClassName("characterBoxSpace")[i].addEventListener("mouseleave", event => {
+
+            const aura = document.getElementById("aura");
+            clearInterval(opacityAuraSetinterval);
+            aura.style.opacity = 0;
+
+        })
+
+        document.getElementsByClassName("characterBoxSpace")[i].addEventListener("click", event => {
+            const selectedImageIcon = document.getElementById("selectedImageIcon");
+            selectedImageIcon.style.display = "block";
+
+            characterImgSrc = document.getElementsByClassName("characterBoxSpace")[i].firstElementChild.src;
+            characterAlt = document.getElementsByClassName("characterBoxSpace")[i].firstElementChild.alt;
+
+            switch (i) {
+
+                case 0:
+                    selectedImageIcon.style.top = "0px";
+                    selectedImageIcon.style.left = "347px";
+                    break;
+                case 1:
+                    selectedImageIcon.style.top = "0px";
+                    selectedImageIcon.style.left = "768px";
+                    break;
+                case 2:
+                    selectedImageIcon.style.top = "0px";
+                    selectedImageIcon.style.left = "1187px";
+                    break;
+
+                //Characters 2nd row
+
+                case 3:
+                    selectedImageIcon.style.top = "269px";
+                    selectedImageIcon.style.left = "347px";
+                    break;
+
+                case 4:
+                    selectedImageIcon.style.top = "269px";
+                    selectedImageIcon.style.left = "768px";
+                    break;
+
+                case 5:
+                    selectedImageIcon.style.top = "269px";
+                    selectedImageIcon.style.left = "1187px";
+                    break;
+            }
+            document.getElementById("chooseCharacterText").innerHTML = "Press Any Key To Continue";
+
+            window.addEventListener("keydown", loadGameplayScreenEvent);
+
+        });
+
+    }
+}
+
+const closeGameplayScreen = () => {
+
+}
+
+const loadGameplayScreen = () => {
+
+    document.getElementById("main").style.display = "block";
+    document.getElementById("characterImg").src = characterImgSrc;
+    // window.removeEventListener("keydown",);
+    console.log("Loaded");
     moveFunction = (e) => {
-
-
+        console.log("Pressed key");
         if (e.code == "Space") { //or keyIsFalse cause keyup
 
             if (timerJump === null) {
@@ -77,13 +250,13 @@ class Character {
 
         if (e.code == "ArrowRight") {
 
-
             let currentLeftPosition = parseInt(getComputedStyle(characterDiv).left);
             characterImg.style.transform = "scaleX(1)";
 
             hitBoxCharacterStyle.style.left = "35px";
             if (currentLeftPosition <= parseInt(main.width) - 230) {
                 leftPosition += 50;
+                console.log(currentLeftPosition);
                 currentLeftPosition = leftPosition + "px";
                 characterDiv.style.left = currentLeftPosition;
 
@@ -115,19 +288,18 @@ class Character {
         }
 
     };
-}
 
-class Cloud {
-    constructor() {
 
-    }
+    //Not yet
+    window.addEventListener("keydown", () => {
+        moveFunction(event);
+    });
 }
-const newCloud = new Cloud();
 
 
 //* Event Listeners
 
-
+window.addEventListener("keydown", loadChooseCharacterScreenEvent);
 
 timerStartContinue = setInterval(() => {
 
@@ -158,135 +330,3 @@ timerStartContinue = setInterval(() => {
 
 // Move cursor onto the image
 
-for (let i = 0; i < document.getElementsByClassName("characterBoxSpace").length; i++) {
-
-    document.getElementsByClassName("characterBoxSpace")[i].addEventListener("mouseenter", (event) => {
-        // console.log(document.getElementsByClassName("characterBoxSpaceHidden")[i].id);
-        const aura = document.getElementById("aura");
-        clearInterval(opacityAuraSetinterval);
-        opacity = 0;
-        aura.style.opacity = opacity;
-
-        opacityAuraSetinterval = setInterval(() => {
-
-
-            aura.style.opacity = opacity;
-
-            if (aura.style.opacity <= 0.6) {
-                opacity = opacity + 0.05;
-                aura.style.opacity = opacity;
-            }
-            else {
-                clearInterval(opacityAuraSetinterval);
-                opacity = 0;
-
-            }
-
-
-
-        }, 100);
-
-        switch (i) {
-
-            case 0:
-                aura.style.top = "-12px";
-                aura.style.left = "56px";
-                break;
-
-            case 1:
-                aura.style.top = "-12px";
-                aura.style.left = "488px";
-                break;
-
-            case 2:
-                aura.style.top = "-12px";
-                aura.style.left = "918px";
-                break;
-
-            //Characters 2nd row
-
-            case 3:
-                aura.style.top = "255px";
-                aura.style.left = "56px";
-                break;
-
-            case 4:
-                aura.style.top = "255px";
-                aura.style.left = "494px";
-                break;
-
-            case 5:
-                aura.style.top = "255px";
-                aura.style.left = "922px";
-                break;
-        }
-    });
-
-    document.getElementsByClassName("characterBoxSpace")[i].addEventListener("mouseleave", event => {
-
-        const aura = document.getElementById("aura");
-        clearInterval(opacityAuraSetinterval);
-        aura.style.opacity = 0;
-
-    })
-
-    document.getElementsByClassName("characterBoxSpace")[i].addEventListener("mouseleave", event => {
-
-        const aura = document.getElementById("aura");
-        clearInterval(opacityAuraSetinterval);
-        aura.style.opacity = 0;
-
-    })
-
-    document.getElementsByClassName("characterBoxSpace")[i].addEventListener("click", event => {
-        const selectedImageIcon = document.getElementById("selectedImageIcon");
-        selectedImageIcon.style.display = "block";
-
-        switch (i) {
-
-            case 0:
-                selectedImageIcon.style.top = "0px";
-                selectedImageIcon.style.left = "347px";
-                break;
-            case 1:
-                selectedImageIcon.style.top = "0px";
-                selectedImageIcon.style.left = "768px";
-                break;
-            case 2:
-                selectedImageIcon.style.top = "0px";
-                selectedImageIcon.style.left = "1187px";
-                break;
-
-            //Characters 2nd row
-
-            case 3:
-                selectedImageIcon.style.top = "269px";
-                selectedImageIcon.style.left = "347px";
-                break;
-
-            case 4:
-                selectedImageIcon.style.top = "269px";
-                selectedImageIcon.style.left = "768px";
-                break;
-
-            case 5:
-                selectedImageIcon.style.top = "269px";
-                selectedImageIcon.style.left = "1187px";
-                break;
-        }
-        document.getElementById("chooseCharacterText").innerHTML = "Press Any Key To Continue";
-
-        window.addEventListener("keydown", () => {
-
-            //TODO: Here the code of the playing screen is loaded.
-
-            const newCharacter = new Character(characterImg);
-            window.addEventListener("keydown", () => {
-                newCharacter.moveFunction(event);
-            });
-
-        })
-
-    });
-
-}
